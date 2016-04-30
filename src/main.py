4dfa686bbcb8f55d791da4779753a6338a0f1f80
Benjamin Streb <Bstreb@gmail.com>
2016-04-29 18:18:33 -0700

    Add essential algorithm for generating regex
    This generates a regex that will match all samples provided
    It has absolutely no smarts, it just matches the union of all the samples

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
 def generate_regex(samples):
-    return samples[0]
+    characters_list = []
+    for sample in samples:
+        characters_list = extend_characters_list(characters_list, len(sample))
+        for i, character in enumerate(sample):
+            characters_list[i].add(character)
+    return "".join(display_group(characters) for characters in characters_list)
+
+
+def display_group(characters):
+    if len(characters) == 1:
+        template = "{}"
+    else:
+        template = "[{}]"
+    return template.format("".join(sorted(characters)))
+
+
+def extend_characters_list(characters_list, length):
+    extension_length = max(length - len(characters_list), 0)
+    return characters_list + [set() for i in range(extension_length)]
--- src/test_main.py
+++ src/test_main.py
@@ -1,5 +1,17 @@
-from . import main
+import main
 
 
 def test_generate_regex():
     assert main.generate_regex(["a"]) == "a"
+    assert main.generate_regex(["a", "b"]) == "[ab]"
+    assert main.generate_regex(["a", "bb"]) == "[ab]b"
+
+
+def test_display_group():
+    assert main.display_group(set("a")) == "a"
+    assert main.display_group(set("ab")) == "[ab]"
+
+
+def test_extend_characters_list():
+    assert main.extend_characters_list([], 5) == [set() for i in range(5)]
+    assert main.extend_characters_list([{"a"}], 3) == [{"a"}, set(), set()]
